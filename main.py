911fbd5e2ef548acb076733ae08a590d3be24352
hendrik.schulze.broering <hendrik.schulze.broering@googlemail.com>
2024-11-16 18:43:10 +0100

    Finished Task 6 but didn't manage to test yet. Adapted main to execute task 6. Adapted tabular_operations to slightly modify a print statement.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from working_with_tabular_data.tabular_operations import print_counts_per_season, print_avg_num_labels, print_max_num_labels
 from working_with_geospatial_vector_data.geo_parquet_operations import print_avg_num_labels as print_avg_num_labels_geo, print_num_overlapping_patches
+from creating_splits_for_dl.create_splits import save_splits_to_csv
 
 
 def main():
@@ -13,6 +14,11 @@ def main():
     file_path = "./untracked-files/milestone01/geoparquets/"
     print_avg_num_labels_geo(file_path)
     print_num_overlapping_patches(file_path)
+
+    # Task 6: Creating train/test splits for deep learning
+    metadata_path = "./untracked-files/milestone01/metadata.parquet"
+    save_splits_to_csv(
+        metadata_path, output_path="./untracked-files/split.csv")
 
 
 if __name__ == "__main__":
